478ea0c42318a1a74b5ddad45e9bf8bc401fb507
Dom Brailsford <dominicbrailsford@gmail.com>
2020-09-30 05:35:17 -0500

    Nice fill style

--- base/make1DHists.C
+++ base/make1DHists.C
@@ -19,6 +19,8 @@ std::vector<TH1F*> Make1DHists(std::vector<Config1D> const& confs, int norm = 0)
     hists[i_conf]->SetLineStyle(confs[i_conf].linestyle);
     hists[i_conf]->GetXaxis()->SetTitle(confs[i_conf].xaxis);
     hists[i_conf]->GetYaxis()->SetTitle(confs[i_conf].yaxis);
+    hists[i_conf]->SetFillStyle(3003);
+    hists[i_conf]->SetFillColor(confs[i_conf].linecolor);
     hists[i_conf]->GetYaxis()->SetTitleOffset(1.2);
     //confs[i_conf].chain->Scan("Run:Subrun:EventID:RecoShowerDirectionZ[RecoIndexShowerHighestNHits]:CalculateUnitDotProduct(RecoShowerDirectionX[RecoIndexShowerHighestNHits], RecoShowerDirectionY[RecoIndexShowerHighestNHits], RecoShowerDirectionZ[RecoIndexShowerHighestNHits], TrueStartMomX[0], TrueStartMomY[0], TrueStartMomZ[0])","RecoNShowers == 1 && RecoIndexShowerHighestNHits>-999 && TruePDG[RecoTrueIndexPrimaryShowerParticle[RecoIndexShowerHighestNHits]] == 11 && TrueMotherID[RecoTrueIndexPrimaryShowerParticle[RecoIndexShowerHighestNHits]]==0 ");
     //confs[i_conf].chain->Scan("Run:Subrun:EventID:RecoNShowerHits[RecoIndexShowerHighestNHits]:RecoNInitialTrackHits[RecoIndexShowerHighestNHits]:TruePDG[RecoInitialTrackTrueIndexPrimaryShowerParticle[RecoIndexShowerHighestNHits]]:RecoInitialTrackTrueFractionPrimaryParticleHitsInShower[RecoIndexShowerHighestNHits]","RecoNShowers> 0 && RecoIndexShowerHighestNHits>-999 && TruePDG[RecoTrueIndexPrimaryShowerParticle[RecoIndexShowerHighestNHits]] == 11 && TrueMotherID[RecoTrueIndexPrimaryShowerParticle[RecoIndexShowerHighestNHits]]==0 && RecoInitialTrackTrueIndexPrimaryShowerParticle[RecoIndexShowerHighestNHits]>-999");
